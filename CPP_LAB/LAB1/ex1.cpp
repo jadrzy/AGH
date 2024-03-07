d79ff0c364daa7a6efa848eed1066e7845073abe
Jan Drzyzga <jdrzyzga10@gmail.com>
2024-03-07 12:21:15 +0100

    LAB1

--- CPP_LAB/LAB1/ex1.cpp
+++ CPP_LAB/LAB1/ex1.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main(void)
 {
-	int num = 10;
+	int num = 18;
 
 	cout << "Test\n\n";
 
 	print_binary(num);
-
+	print_binary_table(num);
+	print_binary_vector(num);
 	return 0;
 }
--- CPP_LAB/LAB1/ex1_fun.cpp
+++ CPP_LAB/LAB1/ex1_fun.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+#include <vector>
 #include "ex1.h"
 #define SIZE 4
 
+// FUNCTION DECIMAL TO BINARY
 void print_binary(int num)
 {
 	short int bin;
@@ -17,18 +19,13 @@ void print_binary(int num)
 
 	cout << "\n";
 }
-
-
-
-
-
+// FUNCTION DECIMAL TO BINARY USING TABLE
 void print_binary_table(int num)
 {
 	short int bint[SIZE];
 	short int bin;
 	char iteration = 1;
-	cout << "Result:\n";
-	
+	cout << "Result - table:\n";
 	while (num > 0)
 	{
 		bin = num % 2;
@@ -36,11 +33,26 @@ void print_binary_table(int num)
 		num = (num - bin) / 2;
 		iteration++;
 	}
-	
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << bint[i];
 	}
-	
+	cout << "\n";
+}
+// FUNCTION DECIMAL TO BINARY USING VECTOR
+void print_binary_vector(int num)
+{
+	vector < int > bin;
+
+	cout << "Result - vector:\n";
+
+	while (num > 0)
+	{
+		bin.insert(bin.begin(), num % 2);
+		num = (num - num % 2) / 2;
+	}
+	for (unsigned int i = 0 ; i < bin.size(); i++)
+		cout << bin[i];
+
 	cout << "\n";
 }
